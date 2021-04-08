51dd4cf19c07549afce468a03cdc9d53561d6ddd
francescofalconieri <56508000+francescofalconieri@users.noreply.github.com>
2021-04-08 11:40:58 +0200

    Update IST.m

--- IST.m
+++ IST.m
@@ -4,33 +4,46 @@ clear all
 close all
 format shortEng
 
-T = 300; % [K]                           % Absolute Temperature
-Lch = 180e-9; % [m]                       % Starting Channel Length
-Lch_des = 16e-9; % [m]                    % Result Channel Length
-Vdd = 3; % [V]                            % Supply Voltage
-Vth = []; % [V]                            % Threshold Voltage
-Na = 8e15; % [cm-2]                      % Channel Doping
+T = 300; % [K]                              % Absolute Temperature
+Lch = 180e-9; % [m]                         % Starting Channel Length
+Lch_des = 16e-9; % [m]                      % Result Channel Length
+Vdd = 3; % [V]                              % Supply Voltage
+Vth = []; % [V]                             % Threshold Voltage
+Na = 8e15; % [cm-2]                         % Channel Doping
 Nd_LDD = 7e19; % [cm-2]                     % LDD Doping
-Nd_SD = 4e20 ; % [cm-2]                      % S&D Doping
-EOT = 3e-9; % [m]                       % Equivalent Oxide Thickness
-er_SiO2 = 3.9;                      % Relative SiO2 Permittivity
-er_Si = 11.8;                  % Relative Si Permittivity
+Nd_SD = 4e20 ; % [cm-2]                     % S&D Doping
+EOT = 3e-9; % [m]                           % Equivalent Oxide Thickness
+er_SiO2 = 3.9;                              % Relative SiO2 Permittivity
+er_Si = 11.8;                               % Relative Si Permittivity
+er_HfO2 = 23;                               % Relative HfO2 Permittivity
 eps = 8.845e-14; % [F/cm]                   % Absolute Free Space Permittivity
-PhiM = 4.05; % [eV]                       % Metal Work Function
-X = 4.05; % [eV]                          % Silicon Electron Affinity
-ni = 1.08e10; % [cm-2]                % Intrinsic Silicon Carrier Density
-Cox = (eps*er_SiO2*1e-2)/(EOT*1e2); % [F]         % Gate Oxide Capacitance
-q = 1.602e-19; % [C]                     % Electron Elementay Charge
-kB = 8.617e-5; % [eV/K]                    % Boltzmann Constant 
+PhiM = 4.5; % [eV]                          % Metal Work Function
+X = 4.05; % [eV]                            % Silicon Electron Affinity
+ni = 1.08e10; % [cm-2]                      % Intrinsic Silicon Carrier Density
+Cox = (eps*er_SiO2)/(EOT); % [F]       % Gate Oxide Capacitance
+q = 1.602e-19; % [C]                        % Electron Elementay Charge
+kB = 8.617e-5; % [eV/K]                     % Boltzmann Constant 
 vt = kB*T; % [V]                            % Termal Voltage
-Eg = 1.11; % [eV]                           % Silicon Enegy Gap
-TBOX = 50e-9; % [m]                        % BOX Thickness
+Egap = 1.11; % [eV]                         % Silicon Enegy Gap
+TBOX = 50e-9; % [m]                         % BOX Thickness
 TSi = 10e-9; % [m]                          % Silicon Sheet Thickness
 W_spacers = 60e-9;                          % Spacers Width
 H_gate = 180e-9;                            % Gate Height
-k_factor = Lch/Lch_des;
+k_factor = Lch/Lch_des;                     % Scaling Factor
 
-xd_SI_foo = @(Na) sqrt((2*er_Si*eps*2*vt*log(Na/ni))/(q*Na))*1e-2;
+Vth_foo = @(Na) (PhiM - vt.*log(Na./ni) - X - 0.5*Egap) + 2*vt.*log(Na./ni) + (1/Cox).* ...
+                sqrt(2*eps*er_Si*q.*Na.*2*vt.*log(Na./ni));
+
+xd_SI_foo = @(Na) sqrt((2*er_Si*eps*2*vt*log(Na./ni))./(q.*Na))*1e-2;
+Na_base = logspace(15,20,10000);
+xd_SI_plot = xd_SI_foo(Na_base);
+Vth_plot = Vth_foo(Na_base);
+
+figure()
+yyaxis left
+semilogx(Na_base,xd_SI_plot/1e-9,'Linewidth',2.5);
+yyaxis right
+semilogx(Na_base,Vth_plot,'Linewidth',2.5);
 
 xd_SI = xd_SI_foo(Na); 
 
